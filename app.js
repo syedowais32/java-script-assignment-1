27249fbeedd27e6d7d9bd54477e1ca1fccb3b1c0
SyedGulzar786 <153593889+SyedGulzar786@users.noreply.github.com>
2024-02-24 22:06:53 +0500

    code learning

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-var num1 = 2 
-var num2 = 5
-var newNum = num1 + num2
-var newNum2 = num1 - num2
-var newNum3 = num1 * num2
-var newNum4 = num1 / num2
-alert(newNum)
-alert(newNum2)
-alert(newNum3)
-alert(newNum4)
-// this will display the sum of num1 and num2 which is 7 in the alert box. 
+// var num1 = 2 
+// var num2 = 5
+// var newNum = num1 + num2
+// var newNum2 = num1 - num2
+// var newNum3 = num1 * num2
+// var newNum4 = num1 / num2
+// alert(newNum)
+// alert(newNum2)
+// alert(newNum3)
+// alert(newNum4)
+// // this will display the sum of num1 and num2 which is 7 in the alert box. 
 
-var abc ="hello"
-alert(abc) // this will display hello in the alert box. 
+// var abc ="hello"
+// alert(abc) // this will display hello in the alert box. 
 
-// concatenation example:
-var a = "hello"
-var b = "world"
-var c = a + " " + b + "!"
-alert(c) // this will display hello world in the alert box
+// var pass = prompt("Please Enter Your Password");
+// alert("Error! Please enter a valid password.") 
+
+// // concatenation example:
+// var a = "hello"
+// var b = "world"
+// var c = a + " " + b + "!"
+// alert(c) // this will display hello world in the alert box
+
+
+// chapter 1
+
+// Q 1
+var abc = " Respected VISITOR; welcome to our website!"
+alert(abc) 
+
+//   Q 2
+alert("Error! Please enter a valid password.") 
+
+// Q 3
+var xyz = " Welcome to JS Land...\n Happy Coding!"
+alert(xyz)
