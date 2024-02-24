55ef3663240bcbb385c21420bec75abb860e40d7
SyedGulzar786 <153593889+SyedGulzar786@users.noreply.github.com>
2024-02-24 21:45:30 +0500

    frits javascript assignment started

---
+++ app.js
@@ -0,0 +1,20 @@
+var num1 = 2 
+var num2 = 5
+var newNum = num1 + num2
+var newNum2 = num1 - num2
+var newNum3 = num1 * num2
+var newNum4 = num1 / num2
+alert(newNum)
+alert(newNum2)
+alert(newNum3)
+alert(newNum4)
+// this will display the sum of num1 and num2 which is 7 in the alert box. 
+
+var abc ="hello"
+alert(abc) // this will display hello in the alert box. 
+
+// concatenation example:
+var a = "hello"
+var b = "world"
+var c = a + " " + b + "!"
+alert(c) // this will display hello world in the alert box
