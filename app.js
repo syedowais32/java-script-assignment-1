ba321bda21351fd236183c10baaa77f0f8544984
SyedGulzar786 <153593889+SyedGulzar786@users.noreply.github.com>
2024-02-27 13:09:30 +0500

    done

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-// var num1 = 2 
-// var num2 = 5
-// var newNum = num1 + num2
-// var newNum2 = num1 - num2
-// var newNum3 = num1 * num2
-// var newNum4 = num1 / num2
-// alert(newNum)
-// alert(newNum2)
-// alert(newNum3)
-// alert(newNum4)
-// // this will display the sum of num1 and num2 which is 7 in the alert box. 
-
-// var abc ="hello"
-// alert(abc) // this will display hello in the alert box. 
-
-// var pass = prompt("Please Enter Your Password");
-// alert("Error! Please enter a valid password.") 
-
-// // concatenation example:
-// var a = "hello"
-// var b = "world"
-// var c = a + " " + b + "!"
-// alert(c) // this will display hello world in the alert box
-
 
 // chapter 1
 
@@ -30,8 +6,69 @@ var abc = " Respected VISITOR; welcome to our website!"
 alert(abc) 
 
 //   Q 2
+
+var abc ="hello"
+alert(abc) // this will display hello in the alert box. 
+
+var pass = prompt("Please Enter Your Password");
 alert("Error! Please enter a valid password.") 
 
 // Q 3
 var xyz = " Welcome to JS Land...\n Happy Coding!"
-alert(xyz)
+alert(xyz) 
+
+// q4
+var shah = "welcome to JS land."
+alert(shah)
+var shahmeer = "prevent this page from creating additional dialogue."
+alert(shahmeer)
+                  
+            //  chapter 2
+// q1
+var userName = "syed gulzar 786"
+alert(userName)
+// q2
+var myName = "syed gulzar-e-mohammad jilani"
+document.write("<h1>welcome "+myName+"</h1>")
+alert(myName)
+// q3
+
+var message ="Hello World"
+alert(message)   
+
+           //  chapter 3
+// q1
+var num1 = 2356
+var num2 = 5567  
+var totalNumber  = num1 + num2 
+var totalNumber1  = num1 - num2 
+var totalNumber2  = num1 * num2 
+var totalNumber3  = num1 / num2 
+
+alert(totalNumber) 
+alert(totalNumber1)         
+alert(totalNumber2)         
+alert(totalNumber3)         
+
+var num1 = 2 
+var num2 = 5
+var newNum = num1 + num2
+var newNum2 = num1 - num2
+var newNum3 = num1 * num2
+var newNum4 = num1 / num2
+alert(newNum)
+alert(newNum2)
+alert(newNum3)
+alert(newNum4)
+// this will display the sum of num1 and num2 which is 7 in the alert box. 
+
+// q2
+alert(234)
+alert(true)
+alert("syed gulzar")
+
+ // // concatenation example:
+var a = "hello"
+var b = "world"
+var c = a + " " + b + "!"
+alert(c) // this will display hello world in the alert box
